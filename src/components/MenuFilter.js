28d9cb5bbca13f30717e857be4838a44efab3cf0
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2022-01-31 16:15:24 +0100

    Remove 1 year

--- src/components/MenuFilter.js
+++ src/components/MenuFilter.js
@@ -11,7 +11,6 @@ const MenuFilter = (props) => {
         <option value="2022">2022</option>
         <option value="2021">2021</option>
         <option value="2020">2020</option>
-        <option value="2019">2019</option>
       </select>
     </div>
   );
